0cf23bc7ff32badcc13a0e3b281cee4d83116fdf
Henry Tan <dragonninja79@gmail.com>
2025-11-02 00:48:03 +0800

    muffins

--- hello.js
+++ hello.js
@@ -1,29 +1,10 @@
 
-let score1=0
-let score2=0;
-let score3=0;
-let height1=170;
-let height2=180;
-let height3=175;
-let age1=50;
-let age2=40;
-let age3=30;
+let X=1;
+let Y=90;
+let Z=10;
 
-score1=height1+5*age1;
-score2=height2+5*age2;
-score3=height3+5*age3;
+let total=0;
 
-console.log(`${score1}, ${score2}, ${score3}`);
+total=(X+(Y/100))*Z;
 
-if(score1>score2&&score1>score3){
-    console.log("Score1 is the winner!");
-}
-else if(score2>score1&&score2>score3){
-    console.log("Score2 is the winner!");
-}
-else if(score3>score2&&score3>score1){
-    console.log("Score3 is the winner!");
-}
-else{
-    console.log("It is a draw!");
-}
+console.log(`Total cost of ${Z} muffins is $${total.toFixed(2)}. $${Math.floor(total)}, ${Math.floor((total.toFixed(2)-Math.floor(total))*100)}cents`);
