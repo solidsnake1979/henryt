9d5d317570805d7835a9f8b21134a1f9260ea429
Henry Tan <dragonninja79@gmail.com>
2025-11-01 23:16:31 +0800

    tutorial1ex4.

--- hello.js
+++ hello.js
@@ -1,6 +1,21 @@
-try{
-console.log("Hello Javascript!");
+
+const add=(num1,num2)=>{
+    console.log(num1+num2);
 }
-catch(error){
-    console.error(error);
+add(2,3);
+
+const sub=(num1,num2)=>{
+    console.log(num1-num2);
 }
+sub(7,4);
+
+const mul=(num1,num2)=>{
+    console.log(num1*num2);
+}
+mul(4,5);
+
+const div=(num1,num2)=>{
+    console.log(num1/num2);
+}
+
+div(10,2);
